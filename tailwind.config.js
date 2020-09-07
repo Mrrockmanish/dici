dc9e7b3018f0c763bbc51de22ac1374818f95512
Mrrockmanish <mrrockmanish@gmail.com>
2020-09-07 07:05:33 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  theme: {
+    screens: {
+      'sm': '576px',
+      'md': '768px',
+      'lg': '992px',
+      'xl': '1200px',
+      'xxl': '1370px'
+    },
+
+    fontSize: {
+      xs: '0.75rem',
+      sm: '0.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+      '7xl': '4.5rem'
+    },
+
+    colors: {
+      'black': '#000000',
+      'white': '#FFFFFF',
+      'dark': '#222327',
+      'dark2': '#303030',
+      'light-dark': '#282A2F',
+      'gray': '#747474',
+      'orange': '#FF9700',
+      'yellow': '#F1C600'
+    }
+
+  },
+
+  corePlugins: {
+    float: false
+  }
+  
+}
